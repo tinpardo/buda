3dfb94153e2687ad4d0cb0d762445d0adc4f8982
tinpardo@gmail.com <tinpardo@gmail.com>
2021-10-04 16:43:53 -0500

    version inicial

---
+++ borrary.py
@@ -0,0 +1,75 @@
+import asyncio
+import json
+
+from binance import AsyncClient, DepthCacheManager, BinanceSocketManager
+
+async def main():
+
+    # initialise the client
+    client = await AsyncClient.create()
+
+    # run some simple requests
+    print(json.dumps(await client.get_exchange_info(), indent=2))
+
+    print(json.dumps(await client.get_symbol_ticker(symbol="BTCUSDT"), indent=2))
+
+    # initialise websocket factory manager
+    bsm = BinanceSocketManager(client)
+
+    # create listener using async with
+    # this will exit and close the connection after 5 messages
+    async with bsm.trade_socket('ETHBTC') as ts:
+        for _ in range(5):
+            res = await ts.recv()
+            print(f'recv {res}')
+
+    # get historical kline data from any date range
+
+    # fetch 1 minute klines for the last day up until now
+    klines = client.get_historical_klines("BNBBTC", AsyncClient.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
+
+    # use generator to fetch 1 minute klines for the last day up until now
+    async for kline in await client.get_historical_klines_generator("BNBBTC", AsyncClient.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"):
+        print(kline)
+
+    # fetch 30 minute klines for the last month of 2017
+    klines = client.get_historical_klines("ETHBTC", Client.KLINE_INTERVAL_30MINUTE, "1 Dec, 2017", "1 Jan, 2018")
+
+    # fetch weekly klines since it listed
+    klines = client.get_historical_klines("NEOBTC", Client.KLINE_INTERVAL_1WEEK, "1 Jan, 2017")
+
+    # setup an async context the Depth Cache and exit after 5 messages
+
+
+    Avance reporte portal monitoreo sitios Sistema Financiero.
+URL para ajustes.
+Ajuste indicador de disponibilidad.
+Por qué no se enviaron alertas por correo ante reporte de caída de 5 min del portal de la SFC.
+Cotización servicio APM.
+    async with DepthCacheManager(client, symbol='ETHBTC') as dcm_socket:
+        for _ in range(5):
+            depth_cache = await dcm_socket.recv()
+            print(f"symbol {depth_cache.symbol} updated:{depth_cache.update_time}")
+            print("Top 5 asks:")
+            print(depth_cache.get_asks()[:5])
+            print("Top 5 bids:")
+            print(depth_cache.get_bids()[:5])
+
+    # Vanilla options Depth Cache works the same, update the symbol to a current one
+    options_symbol = 'BTC-210430-36000-C'
+    async with OptionsDepthCacheManager(client, symbol=options_symbol) as dcm_socket:
+        for _ in range(5):
+            depth_cache = await dcm_socket.recv()
+            count += 1
+            print(f"symbol {depth_cache.symbol} updated:{depth_cache.update_time}")
+            print("Top 5 asks:")
+            print(depth_cache.get_asks()[:5])
+            print("Top 5 bids:")
+            print(depth_cache.get_bids()[:5])
+
+    await client.close_connection()
+
+if __name__ == "__main__":
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ buda_clico_mercados.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import keyboard
+
+mercados = ["btc-cop" , "eth-cop", "ltc-cop"]
+
+
+for x in range(3):
+
+
+    pesos_iniciales = 10000
+
+    #compramos LTC de COP
+    market_id = 'ltc-cop'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_ltc_cop = solicitud["ticker"]["min_ask"][0]
+    cantidad_ltc = int(pesos_iniciales) / float(precio_ltc_cop)
+    print("\n\nCompra de LTC con COP: \t" + str(cantidad_ltc) + " \t\t-- Precio: " + precio_ltc_cop)
+
+
+    #vendemos LTC-CLP
+    market_id = 'ltc-clp'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_ltc_clp = solicitud["ticker"]["max_bid"][0]
+    cantidad_clp = cantidad_ltc * float(precio_ltc_clp)
+    print("Venta LTC a CLP: \t" + str(cantidad_clp) + " \t\t-- Precio: " + precio_ltc_clp)
+
+    #compramos BCH-CLP
+    market_id = 'bch-clp'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_bch_clp = solicitud["ticker"]["min_ask"][0]
+    cantidad_bch = cantidad_clp / float(precio_bch_clp)
+    print("Compra BCH con CLP: \t" + str(cantidad_bch) + " \t\t-- Precio: " + precio_bch_clp)
+
+    #vendemos BCH-COP
+    market_id = 'bch-cop'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_bch_cop = solicitud["ticker"]["max_bid"][0]
+    cantidad_cop = cantidad_bch * float(precio_bch_cop)
+    print("Venta de BCH en COP: \t" + str(cantidad_cop) + " \t\t-- Precio: " + precio_bch_cop)
+
+
+
+
+
+
+    pesos_iniciales = 10000
+
+    #compramos BCH de COP
+    market_id = 'bch-cop'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_bch_cop = solicitud["ticker"]["min_ask"][0]
+    cantidad_bch = int(pesos_iniciales) / float(precio_bch_cop)
+    print("\n\nXXXXXX -----Compra de LTC con COP: \t" + str(cantidad_bch) + " \t\t-- Precio: " + precio_bch_cop)
+
+
+    #vendemos BCH-CLP
+    market_id = 'BCH-clp'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_bc_clp = solicitud["ticker"]["max_bid"][0]
+    cantidad_clp = cantidad_bch * float(precio_bch_clp)
+    print("Venta LTC a CLP: \t" + str(cantidad_clp) + " \t\t-- Precio: " + precio_bch_clp)
+
+    #compramos ETH-CLP
+    market_id = 'eth-clp'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_eth_clp = solicitud["ticker"]["min_ask"][0]
+    cantidad_eth = cantidad_clp / float(precio_eth_clp)
+    print("Compra BCH con CLP: \t" + str(cantidad_eth) + " \t\t-- Precio: " + precio_eth_clp)
+
+    #vendemos BCH-COP
+    market_id = 'eth-cop'
+    url = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
+    solicitud = requests.get(url).json()
+    precio_eth_cop = solicitud["ticker"]["max_bid"][0]
+    cantidad_cop = cantidad_eth * float(precio_eth_cop)
+    print("Venta de BCH en COP: \t" + str(cantidad_cop) + " \t\t-- Precio: " + precio_eth_cop)
+
+    print("\n\n\n" + str(solicitud) + "\n\n\n\n")
